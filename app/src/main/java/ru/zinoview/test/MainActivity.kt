99c0fc19d7d4b73487080993485b9f57cc777ee4
KostyaZinoview <KIZinovev@brs.beeline.ru>
2023-07-11 15:33:53 +0300

    Initial commit

---
+++ app/src/main/java/ru/zinoview/test/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.zinoview.test
+
+import android.os.Build.VERSION_CODES.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("zinoviewk","a is $a")
+        if(savedInstanceState == null) {
+            a = 5
+        }
+
+
+    }
+
+    private companion object {
+        var a = Int.MIN_VALUE
+    }
+}
