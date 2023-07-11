857d5304be94c0f6829598666656d472c899a86d
KostyaZinoview <KIZinovev@brs.beeline.ru>
2023-07-11 16:24:38 +0300

    collapsedc7c8c9

--- app/src/main/java/ru/zinoview/test/MainActivity.kt
+++ app/src/main/java/ru/zinoview/test/MainActivity.kt
@@ -23,6 +23,10 @@ class MainActivity : AppCompatActivity() {
         print(4)
         print(5)
         print(6)
+
+        print(7)
+        print(8)
+        print(9)
     }
 
     private companion object {
